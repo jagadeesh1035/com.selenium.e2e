58e7d7931f5d563715b497c458f6572cd7f52683
Jagadeesh Pacha <jagadeesh1035.mail@gmail.com>
2022-01-09 14:34:25 +0530

    Adding test file to repos

---
+++ src/main/java/com/selenium/e2e/TestingTwoRepos.java
@@ -0,0 +1,10 @@
+package com.selenium.e2e;
+
+public class TestingTwoRepos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Everyone I have integrated two repositories");
+	}
+
+}
